0624abc7e5e775bffad650e6f44ba3b601ea8704
darey6270 <darey6270@gmail.com>
2025-06-18 18:35:39 +0200

    update wed 05 2025

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,14 +3,11 @@ import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
       items: 5
     },
@@ -19,87 +16,90 @@ export const Skills = () => {
       items: 3
     },
     tablet: {
-      breakpoint: { max: 1024, min: 464 },
+      breakpoint: { max: 1024, min: 768 },
       items: 2
     },
     mobile: {
-      breakpoint: { max: 464, min: 0 },
+      breakpoint: { max: 768, min: 0 },
       items: 1
     }
   };
 
   return (
-    <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills I used on </h2>
-                        <div className="skill-bx-flex">
-        <div class="column">
-            <h2>Frontend</h2>
-            <ul>
-              <li>Html</li>
-              <li>Css</li>
-              <li>Javascript</li>
-              <li>Xml</li>
-              <li>Jquery</li>
-              <li>React</li>
-              <li>Angular</li>
-            </ul>
-        </div>
-        <div class="column">
-            <h2>backend</h2>
-            <ul>
-              <li>Nodejs</li>
-              <li>Python/Django</li>
-              <li>Php core/Laravel</li>
-              <li>.Net Framework</li>
-              <li>ASp.net</li>
-              <li>Flask</li>
-            </ul>
-        </div>
-        <div class="column">
-        <h2>Database</h2>
-            <ul>
-              <li>Mongodb</li>
-              <li>SQLlite</li>
-              <li>Mysql</li>
-              <li>postSql</li>
-            </ul>
-        </div>
-    </div>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Web Developer</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Data Analyst</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Data Scientist</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Frontend Developer</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Backend Developer</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Mobibe Developer</h5>
-                            </div>
-                        </Carousel>
-                    </div>
-                </div>
+    <section className="skill-section" id="skills">
+      <div className="container">
+        <div className="skills-wrapper">
+          <h2>Skills I used on</h2>
+
+          <div className="skills-grid">
+            <div className="column">
+              <h3>Frontend</h3>
+              <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>XML</li>
+                <li>jQuery</li>
+                <li>React</li>
+                <li>Angular</li>
+              </ul>
             </div>
+            <div className="column">
+              <h3>Backend</h3>
+              <ul>
+                <li>Node.js</li>
+                <li>Python/Django</li>
+                <li>PHP (Core/Laravel)</li>
+                <li>.NET Framework</li>
+                <li>ASP.NET</li>
+                <li>Flask</li>
+              </ul>
+            </div>
+            <div className="column">
+              <h3>Database</h3>
+              <ul>
+                <li>MongoDB</li>
+                <li>SQLite</li>
+                <li>MySQL</li>
+                <li>PostgreSQL</li>
+              </ul>
+            </div>
+          </div>
+
+          <Carousel
+            responsive={responsive}
+            infinite={true}
+            className="skill-carousel"
+            autoPlay
+            autoPlaySpeed={3000}
+          >
+            <div className="item">
+              <img src={meter1} alt="Web Developer" />
+              <h5>Web Developer</h5>
+            </div>
+            <div className="item">
+              <img src={meter2} alt="Data Analyst" />
+              <h5>Data Analyst</h5>
+            </div>
+            <div className="item">
+              <img src={meter3} alt="Data Scientist" />
+              <h5>Data Scientist</h5>
+            </div>
+            <div className="item">
+              <img src={meter1} alt="Frontend Developer" />
+              <h5>Frontend Developer</h5>
+            </div>
+            <div className="item">
+              <img src={meter1} alt="Backend Developer" />
+              <h5>Backend Developer</h5>
+            </div>
+            <div className="item">
+              <img src={meter1} alt="Mobile Developer" />
+              <h5>Mobile Developer</h5>
+            </div>
+          </Carousel>
         </div>
-        {/* <img className="background-image-left" src={colorSharp} alt="Image" /> */}
+      </div>
     </section>
-  )
-}
+  );
+};
